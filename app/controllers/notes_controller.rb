f180b42a29cdc8ad5624f2313117e149c7829cee
Berry  Semexan <bsemexan@gmail.com>
2018-04-13 01:56:46 +0000

    automatically backed up by learn

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,11 +1,11 @@
 class NotesController < ApplicationController
-  
+
 
   def create
     note = Note.new(note_params)
     note.user = current_user
     note.save!
-    redirect_to notes_path
+    redirect_to '/'
   end
 
   def update
