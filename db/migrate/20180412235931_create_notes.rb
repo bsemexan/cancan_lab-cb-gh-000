688a23c94adb5e37ea7fae6d8597945487414212
Berry  Semexan <bsemexan@gmail.com>
2018-04-13 00:02:45 +0000

    automatically backed up by learn

--- db/migrate/20180412235931_create_notes.rb
+++ db/migrate/20180412235931_create_notes.rb
@@ -1,6 +1,8 @@
 class CreateNotes < ActiveRecord::Migration
   def change
     create_table :notes do |t|
+      t.string :content
+      t.integer :user_id
 
       t.timestamps null: false
     end
